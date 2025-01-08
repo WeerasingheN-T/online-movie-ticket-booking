861dd9b2464995fb265f4f6ac315e454f0c5c24b
Weerasinghe-NT-100 <it20017392@my.sliit.lk>
2025-01-08 13:57:57 +0530

    Modified react .js files

--- online-ticket-management-tool/src/ProductionTeam/ViewProduction.js
+++ online-ticket-management-tool/src/ProductionTeam/ViewProduction.js
@@ -1,9 +1,9 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './AddMovie.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Row,Col} from 'react-bootstrap';
-import {useHistory} from "react-router";
+import { Row, Col } from 'react-bootstrap';
+import { useHistory } from "react-router";
 import { useParams } from 'react-router-dom';
 import AddFi from '../ProductionTeamTools/AddFi.png';
 import EditFi from '../ProductionTeamTools/EditFi1.png';
@@ -11,162 +11,163 @@ import viewFi from '../ProductionTeamTools/viewFi1.jpg';
 
 function ViewTheMovie() {
 
-    const {email}=useParams("");
-    const [teamList,setTeamlist]=useState([]);
+  const { email } = useParams("");
+  const [teamList, setTeamlist] = useState([]);
 
-    var array;
-    var res;
-    var res1;
-    
-    const histoy=useHistory();
+  var array;
+  var res;
+  var res1;
 
-    useEffect(()=>{
+  const histoy = useHistory();
 
-      function getTeams(){
-        axios.get(`http://localhost:8073/team/get/${email}`).then((res)=>{
-          setTeamlist(res.data);
-        }).catch((err)=>{
-          alert(err.message);
-        })
+  useEffect(() => {
+
+    function getTeams() {
+      axios.get(`http://localhost:8073/team/get/${email}`).then((res) => {
+        setTeamlist(res.data);
+      }).catch((err) => {
+        alert(err.message);
+      })
+    }
+    getTeams();
+
+  }, [email]);
+
+  const deleteMovie = async (_id) => {
+    const res2 = await fetch(`http://localhost:8073/team/delete/${_id}`, {
+
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
       }
-      getTeams();
+    });
 
-       },[email]);
+    const deleteData = await res2.json();
+    console.log(deleteData);
 
-       const deleteMovie=async(_id)=>{
-      const res2=await fetch(`http://localhost:8073/team/delete/${_id}`,{
-
-      method:"DELETE",
-      headers:{
-        "Content-Type":"application/json"
-      }
-      });
-
-      const deleteData=await res2.json();
-      console.log(deleteData);
-
-      if(res2.status===500||deleteData){
-        console.log("error");
-      }
-
-      else{
-        alert("Account deleted");
-        histoy.push("/");
-      }
+    if (res2.status === 500 || deleteData) {
+      console.log("error");
     }
 
-       var viewItems_HTMLTABLE="";
+    else {
+      alert("Account deleted");
+      histoy.push("/");
+    }
+  }
 
-       if (!Array.isArray(teamList)) {
+  var viewItems_HTMLTABLE = "";
 
-      array=Object.entries(teamList);
-      res=Object.values(array);
-      res1=Object.values(res[1]);
-      console.log(res1[1]);
+  if (!Array.isArray(teamList)) {
 
-      viewItems_HTMLTABLE=
-        res1.map((data)=>{
+    array = Object.entries(teamList);
+    res = Object.values(array);
+    res1 = Object.values(res[1]);
+    console.log(res1[1]);
 
-          if (typeof(data.imageFiles) !== 'undefined') {
-          
+    viewItems_HTMLTABLE =
+      res1.map((data) => {
 
-        return(
-          
-          <div key={data._id} className="card4 m-2" >
+        if (typeof (data.imageFiles) !== 'undefined') {
 
-         <p className="text-center h1 fw-bold mb-3 mx-1 mx-md-4 mt-4">Production Team Member {data.firstName}'s Account</p>
- 
-          <br/>
 
-        <div className="col-md-8">
-      <Row>
-        <Col><img className="rounded-circle" width="200" height="200" src={require(`../Photos/${data.imageFiles}`)} alt='image4'/></Col>
-      <Col>
-      <Row>
-      <Col> <p className="text-center h2 fw-bold mb-3 mx-1 mx-md-3 mt-6">{data.firstName}&emsp;{data.lastName}</p></Col>
-       </Row>   
-      </Col> 
-      </Row> 
-      
+          return (
+
+            <div key={data._id} className="card4 m-2" >
+
+              <p className="text-center h1 fw-bold mb-3 mx-1 mx-md-4 mt-4">Production Team Member {data.firstName}'s Account</p>
+
+              <br />
+
+              <div className="col-md-8">
+                <Row>
+                  <Col><img className="rounded-circle" width="200" height="200" src={require(`../Photos/${data.imageFiles}`)} alt='image4' /></Col>
+                  <Col>
+                    <Row>
+                      <Col> <p className="text-center h2 fw-bold mb-3 mx-1 mx-md-3 mt-6">{data.firstName}&emsp;{data.lastName}</p></Col>
+                    </Row>
+                  </Col>
+                </Row>
+
+              </div>
+              <div className="card-body">
+                <div className="col-md-8">
+
+                  <Row>
+                    <Col ><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-6">Member Id:</p></Col>
+                    <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-6">{data.MemberId}</p></Col>
+                  </Row>
+                  <Row className="row-md-8">
+                    <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Email:</p></Col>
+                    <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.email}</p></Col>
+                  </Row>
+                  <Row className="row-md-8">
+                    <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">User Name:</p></Col>
+                    <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.email}</p></Col>
+                  </Row>
+                </div>
+              </div>
+              <div className="light">
+                <a href={`/UpdateProductionTeam/${data.email}`}><button className="btn btn-dark btn-lg btn-block">Update Details</button></a>
+                <button className="btn btn-dark btn-lg btn-block" onClick={() => deleteMovie(data._id)}>Delete Account</button>
+              </div>
+            </div>
+
+          );
+        }
+        return null;
+      });
+
+  }
+
+  else
+    viewItems_HTMLTABLE = "loading"
+
+
+
+  return (
+    <>
+
+      <div className="addl2">
+
+
+        {viewItems_HTMLTABLE}
+
+
+        <div className="light1">
+
+          <div className="card-group m-2">
+            <div className="card">
+              <center><img width="200" height="200" src={AddFi} alt='image5' /></center>
+              <div className="card-body">
+                <h2 className="card-title">Add Movies</h2>
+                <p className="card-text">Add new showing movies under the different theaters into the movie collections.</p>
+                <a href="/AddMovie" className="btn btn-secondary">Add Movies</a>
+              </div>
+            </div>
+            <div className="card">
+              <center><img width="200" height="200" src={viewFi} alt='image6' /></center>
+              <div className="card-body">
+                <h2 className="card-title">View Movies</h2>
+                <p className="card-text">There are the showing movies under different theaters</p>
+                <a href="/ViewMovie" className="btn btn-secondary">View Movies</a>
+              </div>
+            </div>
+            <div className="card">
+              <center><img width="300" height="240" src={EditFi} alt='image7' /></center>
+              <div className="card-body">
+                <h2 className="card-title">View Theaters</h2>
+                <p className="card-text">There are the registered theaters and their details.</p>
+                <a href="/ViewMovie" className="btn btn-secondary">View Theaters</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
-      <div className="card-body">
-        <div className="col-md-8">
-        
-        <Row>
-        <Col ><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-6">Member Id:</p></Col> 
-        <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-6">{data.MemberId}</p></Col>
-        </Row>
-       <Row className="row-md-8">
-         <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Email:</p></Col>
-         <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.email}</p></Col>
-         </Row>
-        <Row className="row-md-8">
-          <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">User Name:</p></Col>
-          <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.email}</p></Col>
-          </Row>  
-       </div>   
-    </div>
-    <div className="light">
-    <a href={`/UpdateProductionTeam/${data.email}`}><button className="btn btn-dark btn-lg btn-block">Update Details</button></a>
-    <button className="btn btn-dark btn-lg btn-block" onClick={()=>deleteMovie(data._id)}>Delete Account</button>
-    </div>
-  </div>
 
-        );
-          }
-      });
+    </>
+  );
 
-    }
-
-    else
-    viewItems_HTMLTABLE="loading"
-
-    
-    
-return (
- <>
-
-<div className="addl2">
-
-
-{viewItems_HTMLTABLE}
-
-
-<div className="light1">
-
-<div className="card-group m-2">
-  <div className="card">
-  <center><img width="200" height="200" src={AddFi} alt='image5'/></center>
-    <div className="card-body">
-      <h2 className="card-title">Add Movies</h2>
-      <p className="card-text">Add new showing movies under the different theaters into the movie collections.</p>
-      <a href="/AddMovie" className="btn btn-secondary">Add Movies</a>
-    </div>
-  </div>
-  <div className="card">
-    <center><img width="200" height="200" src={viewFi} alt='image6'/></center>
-    <div className="card-body">
-      <h2 className="card-title">View Movies</h2>
-      <p className="card-text">There are the showing movies under different theaters</p>
-      <a href="/ViewMovie" className="btn btn-secondary">View Movies</a>
-    </div>
-  </div>
-  <div className="card">
-    <center><img width="300" height="240" src={EditFi} alt='image7'/></center>
-    <div className="card-body">
-      <h2 className="card-title">View Theaters</h2>
-      <p className="card-text">There are the registered theaters and their details.</p>
-      <a href="/ViewMovie" className="btn btn-secondary">View Theaters</a>
-    </div>
-  </div>
- </div>
-
-</div> 
-</div>
-
- </>
- );
-    
 }
 
 export default ViewTheMovie;
--- online-ticket-management-tool/src/ProductionTeam/ViewTheMovie.js
+++ online-ticket-management-tool/src/ProductionTeam/ViewTheMovie.js
@@ -1,120 +1,119 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './AddMovie.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Row,Col} from 'react-bootstrap';
-import { useParams} from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 
 function ViewTheMovie() {
 
-    const {id}=useParams("");
-    const [movieList,setMovielist]=useState([]);
+  const { id } = useParams("");
+  const [movieList, setMovielist] = useState([]);
 
-    var arr;
-    var res;
-    var res1;
-    
-    useEffect(()=>{
+  var arr;
+  var res;
+  var res1;
 
-      function getMovies(){
-        axios.get(`http://localhost:8073/movie/get/${id}`).then((res)=>{
-         setMovielist(res.data);
-        }).catch((err)=>{
-          alert(err.message);
-        })
-      }
-      getMovies();
+  useEffect(() => {
 
-       },[id]);
+    function getMovies() {
+      axios.get(`http://localhost:8073/movie/get/${id}`).then((res) => {
+        setMovielist(res.data);
+      }).catch((err) => {
+        alert(err.message);
+      })
+    }
+    getMovies();
 
-       var viewItems_HTMLTABLE="";
+  }, [id]);
 
-       if (!Array.isArray(movieList)) {
+  var viewItems_HTMLTABLE = "";
 
-      arr=Object.entries(movieList);
-      res=Object.values(arr);
-      res1=Object.values(res[1]);
-      console.log(res1[1]);
+  if (!Array.isArray(movieList)) {
 
-      viewItems_HTMLTABLE=
-        res1.map((data)=>{
+    arr = Object.entries(movieList);
+    res = Object.values(arr);
+    res1 = Object.values(res[1]);
+    console.log(res1[1]);
 
-          if (typeof(data.imageFile) !=='undefined') {
-          
+    viewItems_HTMLTABLE =
+      res1.map((data) => {
 
-        return(
-          <div key={data._id} className="card4 m-2" >
-      <div className="row g-0">
-    <div className="col-md-4">
-      <img width="342" height="460" src={require(`../Photo/${data.imageFile}`)} alt='image8'/>
-    </div>
-    <div className="col-md-8">
-      <div className="card-body">
-        <h5 className="text-center fw-bold mb-8 mx-1 mx-md-2 mt-1">{data.MovieName}</h5>
+        if (typeof (data.imageFile) !== 'undefined') {
 
-        <br/><br/>
-        
-        <Row className="row-md-8">
-        <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-6">Movie Id:</p></Col> 
-        <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-6">{data._id}</p></Col>
-        </Row>
-       <Row className="row-md-8">
-         <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Relesed Date:</p></Col>
-         <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.date}</p></Col>
-         </Row>
-        <Row className="row-md-8">
-          <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Movie Description:</p></Col>
-          <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.description}</p></Col>
-          </Row>
-        <Row className="row-md-8">
-          <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Cast:</p></Col>
-          <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.cast}</p></Col>
-          </Row>
-        <Row className="row-md-8">
-          <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Theater:</p></Col>
-          <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.theater}</p></Col>
-          </Row>
-        <Row className="row-md-8">
-          <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Show Days</p></Col>
-        <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.dates}</p></Col>
-        </Row>
-        <Row className="row-md-8">
-          <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Show Time</p></Col>
-          <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.time}</p></Col>
-          </Row>
-      </div>
-    </div>
-    </div>
 
-     <div className="light">
-       <a href={`/EditMovie/${data._id}`}><button className="btn btn-dark btn-lg btn-block">Update Movie</button></a>
-     </div>
-   </div>
+          return (
+            <div key={data._id} className="card4 m-2" >
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img width="342" height="460" src={require(`../Photo/${data.imageFile}`)} alt='image8' />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="text-center fw-bold mb-8 mx-1 mx-md-2 mt-1">{data.MovieName}</h5>
 
-  
-        );
-          }
+                    <br /><br />
+
+                    <Row className="row-md-8">
+                      <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-6">Movie Id:</p></Col>
+                      <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-6">{data._id}</p></Col>
+                    </Row>
+                    <Row className="row-md-8">
+                      <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Relesed Date:</p></Col>
+                      <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.date}</p></Col>
+                    </Row>
+                    <Row className="row-md-8">
+                      <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Movie Description:</p></Col>
+                      <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.description}</p></Col>
+                    </Row>
+                    <Row className="row-md-8">
+                      <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Cast:</p></Col>
+                      <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.cast}</p></Col>
+                    </Row>
+                    <Row className="row-md-8">
+                      <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Theater:</p></Col>
+                      <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.theater}</p></Col>
+                    </Row>
+                    <Row className="row-md-8">
+                      <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Show Days</p></Col>
+                      <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.dates}</p></Col>
+                    </Row>
+                    <Row className="row-md-8">
+                      <Col><p className="text-center h12 fw-bold mb-3 mx-1 mx-md-3 mt-2">Show Time</p></Col>
+                      <Col><p className="text-center h12 mb-3 mx-1 mx-md-3 mt-2">{data.time}</p></Col>
+                    </Row>
+                  </div>
+                </div>
+              </div>
+
+              <div className="light">
+                <a href={`/EditMovie/${data._id}`}><button className="btn btn-dark btn-lg btn-block">Update Movie</button></a>
+              </div>
+            </div>
+          );
+        }
+        return null;
       });
 
-    }
+  }
 
-    else
-    viewItems_HTMLTABLE="loading"
-    
-return (
- <>
+  else
+    viewItems_HTMLTABLE = "loading"
 
-<div className="images4">
+  return (
+    <>
 
-
-{viewItems_HTMLTABLE}
+      <div className="images4">
 
 
+        {viewItems_HTMLTABLE}
 
-</div> 
- </>
- );
-    
+
+
+      </div>
+    </>
+  );
+
 }
 
 export default ViewTheMovie;
